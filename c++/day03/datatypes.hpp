e291a5ac44070edc93b34d81c440b28fa5d9d502
acerne <a.cerne89@gmail.com>
2021-12-10 21:41:07 +0100

    Added the solution to day03 part2

--- c++/day03/datatypes.hpp
+++ c++/day03/datatypes.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <stdint.h>
 #include <vector>
 
-using Input = std::vector<std::vector<bool>>;
+struct Input {
+  std::vector<uint16_t> data;
+  size_t n_bits;
+};
 using Result = int32_t;
 
 static int DAY_ = 3;
--- c++/day03/solution.cpp
+++ c++/day03/solution.cpp
@@ -1,66 +1,81 @@
 #include "datatypes.hpp"
 #include "day.hpp"
 
+#include <bitset>
 #include <fstream>
+#include <numeric>
 #include <sstream>
 
 namespace
 {
 
-std::vector<bool> parse_line(const std::string& line) {
-  std::vector<bool> bits;
-  for (const auto& c : line) {
-    bits.push_back(c != '0');
-  }
-  std::reverse(bits.begin(), bits.end());
-  return bits;
+uint16_t parse_line(const std::string& line) {
+  std::bitset<16> bits(std::stoi(line, nullptr, 2));
+  return bits.to_ulong();
 }
 
-std::vector<size_t> count(const Input& values) {
-  size_t n_bits = values.front().size();
-  std::vector<size_t> true_count(n_bits, 0);
-  for (const auto& item : values) {
-    for (size_t i = 0; i < n_bits; i++) {
-      if (item.at(i)) {
-        true_count.at(i)++;
-      }
-    }
-  }
-  return true_count;
+std::pair<size_t, size_t> count_bit(const Input& input, size_t bit) {
+  auto trues = std::count_if(input.data.begin(), input.data.end(),
+                             [bit](auto& item) { return bool(item & (1 << bit)) == true; });
+  return {trues, input.data.size() - trues};
 }
 
-std::pair<int16_t, int16_t> decode(const std::vector<size_t>& true_count, size_t total) {
-  size_t n_bits = true_count.size();
-  int16_t gamma = 0;
-  for (size_t i = 0; i < n_bits; i++) {
-    if (true_count.at(i) > total / 2) {
+std::pair<uint16_t, uint16_t> decode_p1(const Input& input) {
+  uint16_t gamma = 0;
+  for (size_t i = 0; i < input.n_bits; i++) {
+    const auto [t, f] = count_bit(input, i);
+    if (t > f) {
       gamma |= 1 << i;
     }
   }
-  int16_t epsilon = ~(gamma | (0xFFFF << n_bits));
+  uint16_t epsilon = ~(gamma | (0xFFFF << input.n_bits));
   return {gamma, epsilon};
+}
+
+uint16_t decode_p2(const Input& input, bool search_most_common) {
+  auto candidates(input);
+  int32_t bit = input.n_bits - 1;
+  while (bit >= 0) {
+    const auto [t, f] = count_bit(candidates, bit);
+    bool search = t >= f != search_most_common;
+    candidates.data.erase(
+      std::remove_if(candidates.data.begin(), candidates.data.end(),
+                     [bit, search](auto& item) { return bool(item & (1 << bit)) == search; }),
+      candidates.data.end());
+
+    if (candidates.data.size() == 1) {
+      return candidates.data.front();
+    }
+    bit--;
+  }
+  return 0;
 }
 
 } // namespace
 
 Input Day::read(const std::string& file_path) {
-  Input data;
+  Input input;
   std::ifstream line_stream(file_path);
   std::string line;
   while (std::getline(line_stream, line)) {
-    data.push_back(parse_line(line));
+    if (input.data.empty()) {
+      input.n_bits = line.size();
+    }
+    input.data.push_back(parse_line(line));
   }
-  return data;
+  return input;
 }
 
 Result Day::Part1::process(const Input& input) {
-  const auto counts = count(input);
-  const auto [gamma, epsilon] = decode(counts, input.size());
+  const auto [gamma, epsilon] = decode_p1(input);
   return gamma * epsilon;
 }
 
 Result Day::Part2::process(const Input& input) {
-  return -1;
+  const auto oxygen_generator_rating = decode_p2(input, true);
+  const auto co2_scrubber_rating = decode_p2(input, false);
+
+  return oxygen_generator_rating * co2_scrubber_rating;
 }
 
 extern "C" Day* create_object(int num) {
