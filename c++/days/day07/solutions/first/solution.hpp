aa33026c4bda8768b19bce485db0576d0534dd7d
acerne <a.cerne89@gmail.com>
2021-12-12 16:46:27 +0100

    Added day07 solution

---
+++ c++/days/day07/solutions/first/solution.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+using Input = std::vector<int16_t>;
+using Result = size_t;
+
+static const std::string SOLUTION_NAME_{"first"};
+
+#include "day.hpp"
+#include "result.hpp"
+#include "solution_impl.hpp"
