88fa39f717a5b5d97f6a26db8d493d7cf2d3b3c3
acerne <a.cerne89@gmail.com>
2021-12-17 23:17:02 +0100

    Added day16 solution

---
+++ c++/days/day16/result.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <limits>
+
+static const int NUL = std::numeric_limits<uint64_t>::min();
+
+static Result PART1_SAMPLE_RESULT_ = {16, 12, 23, 31, NUL, NUL, NUL, NUL, NUL, NUL, NUL, NUL};
+static Result PART2_SAMPLE_RESULT_ = {NUL, NUL, NUL, NUL, 3, 54, 7, 9, 1, 0, 0, 1};
---
+++ c++/days/day16/solutions/first/solution.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+using Input = std::vector<std::string>;
+using Result = std::vector<uint64_t>;
+
+static const std::string SOLUTION_NAME_{"first"};
+
+#include "day.hpp"
+#include "result.hpp"
+#include "solution_impl.hpp"
